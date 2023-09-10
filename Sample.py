dfe3f542c5531a3e66457ec5135297858333aa55
hari2979 <blackangel2979@gmail.com>
2023-09-10 08:45:41 +0530

    New files added on 10/09/2023

---
+++ Sample.py
@@ -0,0 +1,4 @@
+a = str("10")
+b = str("10")
+c = b+a
+print (c)
---
+++ Sample2.py
@@ -0,0 +1,6 @@
+a =int(input ())
+b =int(input ())
+c = a+b
+print (c)
+# getting input from user
+# Anything inside input parameters it defaults consider as an string variable so we should define like Integer, String, Float ect.,
